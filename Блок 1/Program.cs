a2047a7de8f574d03edc6880e111f152936970e4
Sedrokin15 <135117798+Sedrokin15@users.noreply.github.com>
2023-06-11 01:38:56 +0300

    Add files via upload

---
+++ Блок 1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Блок_1
+{
+    partial class Program
+    {
+        static void ResizeMass(ref int[] array)
+        {
+            Console.WriteLine("Введiть скiльки елементiв треба знищити");
+            int t = int.Parse(Console.ReadLine());
+            Console.WriteLine("Bведiть з якого елемента K");
+            int k = int.Parse(Console.ReadLine());
+            if (k < 0)
+            {
+                Console.WriteLine();
+                return;
+            }
+            int count = 0;
+            for (int i = k - 1; i < array.Length; i++)
+            {
+                if (count != t)
+                {
+                    count++;
+                    array[i] = int.MinValue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            int[] massiv = new int[array.Length - count];
+            count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != int.MinValue)
+                {
+                    massiv[count] = array[i];
+                    count++;
+                }
+            }
+            Output(massiv);
+        }
+
+        static void Output(int[] massiv)
+        {
+            foreach (int i in massiv)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        static int[] RandomMet(ref int number)
+        {
+            Random random = new Random();
+            int[] array = new int[number];
+            for (int i = 0; i < number; i++)
+            {
+                array[i] = random.Next(-101, 101);
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Оберіть спосіб вводу 1 - рандомно 2 - вручну");
+            int x = int.Parse(Console.ReadLine());
+            switch (x)
+            {
+                case 1:
+                    Console.WriteLine("Введіть кількість елементів масиву");
+                    int number = int.Parse(Console.ReadLine());
+                    int[] array = new int[number];
+                    array = RandomMet(ref number);
+                    ResizeMass(ref array);
+                    break;
+                case 2:
+                    Console.WriteLine("Введiть масив");
+                    array = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                    ResizeMass(ref array);
+                    break;
+            }
+        }
+    }
+}
---
+++ Блок 2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Блок_2
+{
+    class YourClass
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введіть єдине натуральне число");
+            
+            int n = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[n][];
+            Console.WriteLine("0 : ");
+            for (int i = 1; i < n; i++)
+            {
+                int count = 0;
+                for (int j = 1; j <= n; j++)
+                {
+                    if (j % SumOfNumbers(i) == 0)
+                    {
+                        count++;
+                    }
+                }
+                matrix[i] = new int[count];
+                int a = 0;
+                for (int m = 1; m <= n; m++)
+                {
+                    if (SumOfNumbers(i) == m && i > 9)
+                    {
+                        matrix[i] = matrix[SumOfNumbers(i)];
+                        break;
+                    }
+                    if (m % SumOfNumbers(i) == 0)
+                    {
+                        matrix[i][a] = m;
+                        a++;
+                    }
+                }
+                Console.Write($"{i} : ");
+                PrintCal(matrix[i]);
+                Console.WriteLine();
+            }
+        }
+
+        static int SumOfNumbers(int i)
+        {
+            int sum = 0;
+            do
+            {
+                sum += Math.Abs(i % 10);
+                i = Math.Abs(i / 10);
+            } while (i / 10 != 0);
+            sum += i;
+            return sum;
+        }
+
+        static void PrintCal(int[] arr)
+        {
+            foreach (var element in arr)
+            {
+                Console.Write($"{element} ");
+            }
+        }
+    }
+}
+
